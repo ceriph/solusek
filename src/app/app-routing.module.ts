80e666e855b24700fd772e05da16bb817dc48e97
Matthew Forrest <c2.forrest@gmail.com>
2017-06-04 21:31:22 +0100

    updates race page style for DE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,15 @@ import {PaladinComponent} from "./character/classes/paladin/paladin.component";
 import {ReaverComponent} from "./character/classes/reaver/reaver.component";
 import {ClericComponent} from "./character/classes/cleric/cleric.component";
 import {ShamanComponent} from "./character/classes/shaman/shaman.component";
+import {HumanComponent} from "./character/races/human/human.component";
+import {HighelfComponent} from "./character/races/highelf/highelf.component";
+import {WoodelfComponent} from "./character/races/woodelf/woodelf.component";
+import {DarkelfComponent} from "./character/races/darkelf/darkelf.component";
+import {HalflingComponent} from "./character/races/halfling/halfling.component";
+import {DwarfComponent} from "./character/races/dwarf/dwarf.component";
+import {OgreComponent} from "./character/races/ogre/ogre.component";
+import {TrollComponent} from "./character/races/troll/troll.component";
+import {IksarComponent} from "./character/races/iksar/iksar.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -20,6 +29,15 @@ const routes: Routes = [
   { path: 'players/:name', component: PlayerDetailComponent },
   { path: 'rules', component: RulesComponent },
   { path: 'lore', component: LoreComponent },
+  { path: 'races/human', component: HumanComponent },
+  { path: 'races/highelf', component: HighelfComponent },
+  { path: 'races/woodelf', component: WoodelfComponent },
+  { path: 'races/darkelf', component: DarkelfComponent },
+  { path: 'races/dwarf', component: DwarfComponent },
+  { path: 'races/halfling', component: HalflingComponent },
+  { path: 'races/ogre', component: OgreComponent },
+  { path: 'races/troll', component: TrollComponent },
+  { path: 'races/iksar', component: IksarComponent },
   { path: 'classes/warrior', component: WarriorComponent },
   { path: 'classes/paladin', component: PaladinComponent },
   { path: 'classes/reaver', component: ReaverComponent },
