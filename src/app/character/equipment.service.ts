e5cac58473968f58ba8b10ea82bc099c299784e9
Matt Forrest <Matt Forrest>
2017-06-29 20:02:31 +0100

    Fixed incorrect imports

--- src/app/character/equipment.service.ts
+++ src/app/character/equipment.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {AngularFireDatabase, FirebaseObjectObservable} from "angularfire2/database";
-import {Armour, ArmourBase, Equipment, WeaponBase} from "./equipment";
+import {Equipment} from "./equipment";
 
 @Injectable()
 export class EquipmentService {
@@ -8,16 +8,6 @@ export class EquipmentService {
   path = 'equipment';
 
   constructor(private db: AngularFireDatabase) {}
-
-  getBaseArmour(name: string): FirebaseObjectObservable<ArmourBase> {
-    console.log("Getting armour:", name);
-    return this.db.object(this.path + '/basearmour/' + name);
-  }
-
-  getBaseWeapon(name: string): FirebaseObjectObservable<WeaponBase> {
-    console.log("Getting weapon:", name);
-    return this.db.object(this.path + '/baseweapons/' + name);
-  }
 
   get(name: string): FirebaseObjectObservable<Equipment> {
     console.log("Getting equipment:", name);
